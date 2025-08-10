6e7206cf23d3bf62e294ee153ec996bc96ea761b
thxmasj <thxmasj@gmail.com>
2025-08-10 15:01:04 +0200

    Removed unused card payment events

--- templates/src/main/java/com/github/thxmasj/statemachine/templates/cardpayment/PaymentEvent.java
+++ templates/src/main/java/com/github/thxmasj/statemachine/templates/cardpayment/PaymentEvent.java
@@ -76,7 +76,6 @@ public interface PaymentEvent {
   EventType<AuthenticationResult, AuthenticationResult> InvalidPaymentTokenStatus = EventType.of("InvalidPaymentTokenStatus", UUID.fromString("0e15a3a7-ee6d-4ddd-9eaa-5d98bf42d635"), AuthenticationResult.class);
   EventType<AuthenticationResult, AuthenticationResult> InvalidAuthenticationToken = EventType.of("InvalidAuthenticationToken", UUID.fromString("de24bd56-5ad1-4b1e-b567-8eb719c0ff51"), AuthenticationResult.class);
   EventType<AuthenticationResult, AuthenticationResult> AuthorisationRequest = EventType.of("AuthorisationRequest", UUID.fromString("4391c1cf-1cda-4627-9d9d-fe8b18b8b6cb"), AuthenticationResult.class);
-  EventType<AcquirerResponse, AcquirerResponse> AuthorisationAdviceApproved = EventType.of("AuthorisationAdviceApproved", UUID.fromString("fc91e51a-0b0c-4014-9587-fe6d03af3c46"), AcquirerResponse.class);
   EventType<AcquirerResponse, AcquirerResponse> AuthorisationApproved = EventType.of("AuthorisationApproved", UUID.fromString("4a3821a0-dbba-448b-8175-c40e4a771df4"), AcquirerResponse.class);
   EventType<Capture, Capture> CaptureRequest = EventType.of("CaptureRequest", UUID.fromString("b4cef9f9-c9dd-40e4-a627-25ba529aec2e"), Capture.class);
   EventType<AcquirerResponse, AcquirerResponse> CaptureApproved = EventType.of("CaptureApproved", UUID.fromString("6186a241-f9e0-40ae-b444-6ce5e2509dcc"), AcquirerResponse.class);
@@ -84,8 +83,6 @@ public interface PaymentEvent {
   EventType<AcquirerResponse, AcquirerResponse> RefundApproved = EventType.of("RefundApproved", UUID.fromString("9c7f8d63-9b27-4bf3-8a90-b7bea56a3fbd"), AcquirerResponse.class);
   EventType<Void, Void> AuthorisationExpired = EventType.of("AuthorisationExpired", UUID.fromString("8c283db8-742e-4667-a370-45bb0fb3f39e"));
   EventType<AcquirerResponse, AcquirerResponse> AcquirerDeclined = EventType.of("AcquirerDeclined", UUID.fromString("994388bc-73a4-4334-ba16-41d471ee56b1"), AcquirerResponse.class);
-  EventType<AcquirerResponse, AcquirerResponse> BankRespondedIncomprehensibly = EventType.of("BankRespondedIncomprehensibly", UUID.fromString("3350b7b6-4a31-45e2-9585-7522a4697497"), AcquirerResponse.class);
-  EventType<AcquirerResponse, AcquirerResponse> BankRequestFailed = EventType.of("BankRequestFailed", UUID.fromString("3901db73-9647-417b-8a6c-1f81250a03e0"), AcquirerResponse.class);
   EventType<Void, Void> RollbackRequest = EventType.rollback("RollbackRequest", UUID.fromString("da2d5fd5-beb7-4497-87f7-479ba7eb2a66"));
   EventType<Void, Void> Cancel = EventType.cancel("Cancel", UUID.fromString("d7bb2f14-4680-476e-8395-c972a1037589"))
   ;
